69bd24c44570ff1e4c24564fe6992a92d782db99
Brian Surowiec <brian@13degrees.com>
2024-09-23 00:53:54 -0400

    Update eslint to v9

---
+++ types/eslint-plugin-sort-destructure-keys.d.ts
@@ -0,0 +1,7 @@
+declare module "eslint-plugin-sort-destructure-keys" {
+  import type { ESLint } from "eslint";
+
+  const eslintPluginSortDestructureKeys: ESLint.Plugin;
+
+  export = eslintPluginSortDestructureKeys;
+}
